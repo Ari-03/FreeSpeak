509a0f537ba7a43cb240b924975f85316dcfbe4f
Aritra Das <d.aritra@outlook.com>
2026-09-19 13:37:58 -0700

    Initial Commit

---
+++ FreeSpeak/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FreeSpeak
+//
+//  Created by Aritra Das on 9/19/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FreeSpeak/FreeSpeakApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeSpeakApp.swift
+//  FreeSpeak
+//
+//  Created by Aritra Das on 9/19/26.
+//
+
+import SwiftUI
+
+@main
+struct FreeSpeakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
